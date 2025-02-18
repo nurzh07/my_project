8ae19cfc77375ea3cd6952e79fa06521e2c13797
nurka <nurka2007n@gmail.com>
2025-02-18 13:28:34 +0500

    Добавлен Django-проект hw_3 с приложением todos

--- hw_3/hw_3/settings.py
+++ hw_3/hw_3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96+7m3zdl%gj&11p%&=-p%c@k^ja@5+c9r@kw&#)+(r0q=@3mk'
+SECRET_KEY = 'django-insecure-oq!wwu$#vxxdshtfiwg%2%0jaf&jpwh%tc_$=dl1cz4&&%$pu^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo',
+    'todos',
 ]
 
 MIDDLEWARE = [
--- hw_3/todos/views.py
+++ hw_3/todos/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import JsonResponse
 from .models import Todo
 from .forms import TodoForm
 
 def todo_list(request):
     todos = Todo.objects.all()
-    return render(request, 'todo/todo_list.html', {'todos': todos})
+    return render(request, 'todos/todo_list.html', {'todos': todos})
 
 def todo_detail(request, id):
     todo = get_object_or_404(Todo, id=id)
-    return render(request, 'todo/todo_detail.html', {'todo': todo})
+    return render(request, 'todos/todo_detail.html', {'todo': todo})
 
 def todo_create(request):
     if request.method == 'POST':
@@ -19,7 +18,7 @@ def todo_create(request):
             return redirect('todo_list')
     else:
         form = TodoForm()
-    return render(request, 'todo/todo_form.html', {'form': form})
+    return render(request, 'todos/todo_form.html', {'form': form})
 
 def todo_delete(request, id):
     todo = get_object_or_404(Todo, id=id)
